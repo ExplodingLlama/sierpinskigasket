ab2843283e4ae0ef12259e964fb02228e1afb3b6
Ratandeep Singh <ratan14@gmail.com>
2019-07-03 23:13:54 +0530

    Gasket done

--- src/components/P5Wrapper/gasketsketch/index.js
+++ src/components/P5Wrapper/gasketsketch/index.js
@@ -2,38 +2,25 @@ export default function(s) {
   s.props = {};
   s.onSetAppState = () => {};
 
-  s.setup = function() {
-    s.createCanvas(window.innerWidth, window.innerHeight / 2);
-  };
-
   s.state = {
     width: window.innerWidth,
-    height: window.innerHeight / 2,
-    clipLength: 2000, //in milliseconds
-    currentTime: 0, //always between 0 and 1
-    dots: [], //contains number from 0 to 1
-    notes: [], //contains an object with value of the corresponding dot, and the handle of the tone instrument object
-    timeAtLastDraw: 0
-  };
-
-  s.LINE_WIDTH = 4;
-  s.BAR_HEIGHT = 100;
-  s.LINE_START = (1 / 5) * s.state.width;
-  s.LINE_END = (4 / 5) * s.state.width;
-  s.LINE_Y = (1 / 3) * s.state.height;
-
-  s.getLineXCoordinates = () => {
-    if (
-      s.mouseX >= s.LINE_START &&
-      s.mouseX <= s.LINE_END &&
-      s.mouseY >= s.LINE_Y - 5 &&
-      s.mouseY <= s.LINE_Y + s.LINE_WIDTH + 5
-    ) {
-      let lineX = (s.mouseX - s.LINE_START) / (s.LINE_END - s.LINE_START);
-      lineX = Math.round(lineX * 100) / 100;
-      return lineX;
+    height: (window.innerHeight * 3) / 4,
+    current: {
+      x: window.innerWidth / 2,
+      y: 50
+    },
+    p1: {
+      x: window.innerWidth / 2,
+      y: 50
+    },
+    p2: {
+      x: window.innerWidth / 5,
+      y: (window.innerHeight * 3) / 4 - 50
+    },
+    p3: {
+      x: (window.innerWidth * 4) / 5,
+      y: (window.innerHeight * 3) / 4 - 50
     }
-    return -1;
   };
 
   s.checkForReset = () => {
@@ -43,81 +30,41 @@ export default function(s) {
     }
   };
 
-  s.isThereANote = (oldT, newT) => {
-    var isThere = false;
-    s.state.dots.forEach(dot => {
-      if (dot >= oldT && dot < newT) {
-        isThere = true;
-      }
-    });
-    return isThere;
-  };
+  s.setup = function() {
+    s.createCanvas(window.innerWidth, (window.innerHeight * 3) / 4);
 
-  // s.checkNotes = () => {
-  //   s.state.dots.forEach(dot => {
-  //     let found = s.state.notes.find(element => element.dot === dot);
-  //     if (!found) {
-  //       s.state.notes.push({ dot: dot });
-  //     }
-  //   });
-  // };
-
-  s.handleDotCreationOrDeletion = lineX => {
-    let pos = -1;
-    if ((pos = s.state.dots.indexOf(lineX)) !== -1) {
-      s.state.dots.splice(pos, 1);
-    } else {
-      s.state.dots.push(lineX);
-    }
-  };
-
-  s.mousePressed = () => {
-    let lineX = s.getLineXCoordinates();
-    if (lineX >= 0 && lineX <= 1) {
-      s.handleDotCreationOrDeletion(lineX);
-    }
-    return false;
-  };
-
-  s.draw = function() {
     //setup
     s.background(0);
     s.stroke(255, 221, 89);
     s.fill(255, 221, 89);
-    s.rect(s.LINE_START, s.LINE_Y, s.LINE_END - s.LINE_START, s.LINE_WIDTH, 10);
-    //end setup
-    s.stroke(103, 89, 255);
-    s.fill(103, 89, 255);
-    s.state.dots.forEach(dot => {
-      s.circle(
-        dot * (s.LINE_END - s.LINE_START) + s.LINE_START,
-        s.LINE_Y + s.LINE_WIDTH / 2,
-        10
-      );
-    });
-    //draw the bar
-    s.stroke(255);
-    s.fill(255);
-    let barX = s.state.currentTime * (s.LINE_END - s.LINE_START) + s.LINE_START;
-    s.rect(
-      barX,
-      s.LINE_Y - s.BAR_HEIGHT / 2 + s.LINE_WIDTH / 2,
-      2,
-      s.BAR_HEIGHT
-    );
-    var oldCurrentTime = s.state.currentTime;
-    if (s.props.isPlaying) {
-      let deltaT = s.millis() - s.state.timeAtLastDraw;
-      s.state.currentTime =
-        (deltaT + s.state.currentTime * s.state.clipLength) /
-        s.state.clipLength;
-    }
-    if (s.state.currentTime > 1) {
-      s.state.currentTime = 0 + s.state.currentTime - 1;
-    }
 
-    s.state.timeAtLastDraw = s.millis();
-    // s.checkNotes();
-    s.checkForReset();
+    s.point(s.state.p1.x, s.state.p1.y);
+    s.point(s.state.p2.x, s.state.p2.y);
+    s.point(s.state.p3.x, s.state.p3.y);
+  };
+
+  s.draw = function() {
+    if (s.props.isPlaying) {
+      let current = s.state.current;
+      s.point(current.x, current.y);
+      let r = s.random(3);
+      let newPoint = {
+        x: current.x,
+        y: current.y
+      };
+      if (r < 1) {
+        newPoint.x = (newPoint.x + s.state.p1.x) / 2;
+        newPoint.y = (newPoint.y + s.state.p1.y) / 2;
+      }
+      if (r >= 1 && r < 2) {
+        newPoint.x = (newPoint.x + s.state.p2.x) / 2;
+        newPoint.y = (newPoint.y + s.state.p2.y) / 2;
+      }
+      if (r >= 2 && r < 3) {
+        newPoint.x = (newPoint.x + s.state.p3.x) / 2;
+        newPoint.y = (newPoint.y + s.state.p3.y) / 2;
+      }
+      s.state.current = { ...newPoint };
+    }
   };
 }
